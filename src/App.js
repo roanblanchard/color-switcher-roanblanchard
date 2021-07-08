ae4990a5f2e7e00647e230ff65791c9057a14861
roanblanchard <roan.g.blanchard@icloud.com>
2021-07-08 10:29:43 -0400

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import ColorChangeButton from './ColorChangeButton'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App () {
+    const [color, setColor] = useState('')
+    return (
+    <div className={`react-root ${color}`}>
+        <div className='centered'>
+            <h1>Color Picker</h1>
+            <ColorChangeButton color='red' setColor={setColor} />
+            <ColorChangeButton color='blue' setColor={setColor} />
+            <ColorChangeButton color='yellow' setColor={setColor} />
+        </div>
     </div>
-  );
+    )
 }
 
 export default App;
